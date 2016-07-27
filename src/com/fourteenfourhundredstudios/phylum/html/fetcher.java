721a991ffa9bb2d35d19dc045adad893241b5a25
fourteenfour hundred <xk9wdefrvnr343nfnd@gmail.com>
2016-07-26 21:09:07 -0500

    Parser Changes

--- src/com/fourteenfourhundredstudios/phylum/html/fetcher.java
+++ src/com/fourteenfourhundredstudios/phylum/html/fetcher.java
@@ -9,6 +9,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONObject;
 
 public class fetcher {
 	
@@ -22,6 +27,7 @@ public class fetcher {
 	    PrintWriter writer = null;
 
 	    try {
+	    	/*
 	        url = new URL(urlName);
 	        is = url.openStream();  // throws an IOException
 	        br = new BufferedReader(new InputStreamReader(is));
@@ -29,24 +35,33 @@ public class fetcher {
 	        while ((line = br.readLine()) != null) {
 	            //System.out.println(line);
 	        	totalHtml = totalHtml+"\n"+line;
-	        }
-	        HTMLParser parser = new HTMLParser(totalHtml);
+	        }*/
+	    	 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	  	   //get current date time with Date()
+	  	   Date date = new Date();
+	    	JSONObject data=null;
+	    	try{
+	    		data = new JSONObject("{'url':'"+urlName+"','topics':['general'],'timestamp':'"+dateFormat.format(date)+"'}");
+	    	}catch(Exception e){
+	    		e.printStackTrace();
+	    	}
+	    	
+	        Parser parser = new Parser(urlName);
+	        
 	       	writer = new PrintWriter(sourceCount+".txt", "UTF-8");
-	       	writer.println(parser.getText());
+	       	writer.println(data.toString());
+	       	writer.println(parser.removeHTML());
 	       	//writer.println(totalHtml);
 	       	writer.flush();
         	writer.close();
-        	System.out.println(url+" just got saved");
+        	System.out.println(urlName+" just got saved");
         	System.out.println(sourceCount);
 	        
 	        //just exception handleing down there
-	    } catch (MalformedURLException mue) {
-	         mue.printStackTrace();
+
 	    } catch (IOException ioe) {
 	         ioe.printStackTrace();
-	    } finally {
-
-	    }
+	    } 
 	    return totalHtml;
 	}
 }
