85c0435d2e9454aef5f9f8354bb67b01f5788f60
fourteenfour hundred <xk9wdefrvnr343nfnd@gmail.com>
2016-07-27 15:14:22 -0500

    Web Crawler Changes

--- src/com/fourteenfourhundredstudios/phylum/html/CrawlBot.java
+++ src/com/fourteenfourhundredstudios/phylum/html/CrawlBot.java
@@ -3,11 +3,24 @@ package com.fourteenfourhundredstudios.phylum.html;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.net.*;
  
 public class CrawlBot {
@@ -15,48 +28,63 @@ public class CrawlBot {
 	fetcher f = new fetcher();
 	int numOfSources = 0;
 	int sourceCounter = 0;
+	BufferedReader br = null;
  
     public CrawlBot(){
         //http://en.wikipedia.org/wiki/Java_(programming_language)
         //System.out.println(downloadWebsite("http://wikipedia.org/wiki/Java_(programming_language)"));
         //downloadWebsiteHTTPSOCK("en.wikipedia.org/wiki/Ada_(programming_language)",false);
-        saveAllLinksFrom("http://rcmania.com/",true);
+      
+    	
+    //	saveAllLinksFrom("https://en.wikipedia.org/wiki/Java",true);
+    	getNewLink();
+    //	Util.getHash(",-");
     }
-     
 
-    public static int nthOc(String str, char c, int n) {
-        int pos = str.indexOf(c, 0);
-        while (n-- > 0 && pos != -1)
-            pos = str.indexOf(c, pos+1);
-        return pos;
+    
+    public void getNewLink(){
+    	String url ="";
+    	try {
+    		br = new BufferedReader(new FileReader("Links2Download.txt"));
+    		while((url = br.readLine() )!=""){
+    		//delete that url now here
+    		Util.removeFirstLine("Links2Download.txt");
+    		this.saveAllLinksFrom(url, true);
+    		}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
      
      
     public void saveAllLinksFrom(String url,boolean log){
         try{
-            //String site=downloadWebsiteHTTPSOCK(url,true);
+        	System.out.println(url);
+        	f.getHtml(url,sourceCounter);
+        	Document doc = Jsoup.connect(url).get();
+        	Elements links = doc.select("a[href]");
+     	    for(Element s : links){
+     	    	String link=s.attr("href");
+     	    	System.out.println(link);
+     	    	if(!link.startsWith("#")){
+     	    		if(link.startsWith("/"))link=url+link;
+     	    		System.out.println(link);
+     	    		sourceCounter++;
+                    //f.getHtml(link,sourceCounter);
+     	    		String saveableLink=link+"\n";
+                    Files.write(Paths.get("Links2Download.txt"), saveableLink.getBytes(), StandardOpenOption.APPEND);
 
-        	String site = f.getHtml(url,sourceCounter);
-            site=site.replace(">","<");
-            String[] lines=site.split("<");
-            for(int i=0;i<lines.length;i++){
-                String[] lineParts=lines[i].split(" ");
-                if(lineParts[0].equals("a")){
-                    for(int j=0;j<lineParts.length;j++){
-                        if(lineParts[j].startsWith("href")){
-                            String link=lineParts[j].substring(6,lineParts[j].length()-1);
-                            if(link.startsWith("/")){
-                                link=url.substring(0,nthOc(url, '/', 2))+link;
-                            }
-                            //downloadWebsiteHTTPSOCK(link,true);
-                            sourceCounter++;
-                            f.getHtml(link,sourceCounter);
-                        }
-                    }
-                }
-            }
+     	    	}
+     	    }
+        	
         }catch(Exception e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
+            
         }
     }
      
